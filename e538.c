e1f3d0bf6c047606ad9ba973d9c2caa5e4a40ed8
zerojudge-c <chenyongda328@gmail.com>
2026-03-16 07:47:37 +0800

    Add files via upload

---
+++ e538.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (void)
+{
+	int n,d,r;
+	while(scanf("%d %d %d",n,d,r)==3){
+		if(n==d==r==0){
+			break;
+		}
+		int i,j,money;
+		int early[n],late[n];
+		else if{
+			for(i=0;i<n;i++){
+				if(early[i]+late[i]>d){
+					money=(early[i]+late[i])*r;
+				}
+			}
+		}
+	}
+	return 0;
+} 
+
