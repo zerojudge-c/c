f0e923046bc1f888e7d8812ff410899de5e1d944
zerojudge-c <chenyongda328@gmail.com>
2026-03-16 07:45:10 +0800

    Update e538錯.c

--- e538錯.c
+++ e538錯.c
@@ -3,21 +3,42 @@
 int main (void)
 {
 	int n,d,r;
-	while(scanf("%d %d %d",n,d,r)==3){
-		if(n==d==r==0){
+	while(scanf("%d %d %d",&n,&d,&r)==3){
+		if(n==0&&d==0&&r==0){
 			break;
 		}
-		int i,j;
-		int a[n][2]; 
+		int i,money=0,x,y,temp;
+		int early[n],late[n];
 		for(i=0;i<n;i++){
-			for(j=0;j<2;j++){
-				scanf("%d",a[i][j]);
-				if(a[i][j]+a[i][j]>d){
-					printf("%d",(a[i][j]+a[i][j]-d)*r);
+			scanf("%d",&early[i]);
+		}
+		for(i=0;i<n;i++){
+			scanf("%d",&late[i]);
+		}
+		for(x=0;x<n-1;x++){
+			for(y=x+1;y<n;y++){
+				if(early[x]>early[y]){
+					temp=early[x];
+					early[x]=early[y];
+					early[y]=temp;
 				}
 			}
 		}
-		
+		for(x=0;x<n-1;x++){
+			for(y=x+1;y<n;y++){
+				if(late[x]<late[y]){
+					temp=late[x];
+					late[x]=late[y];
+					late[y]=temp;
+				}
+			}
+		}
+		for(i=0;i<n;i++){
+			if(early[i]+late[i]>d){
+				money+=(early[i]+late[i]-d)*r;
+			}
+		}
+		printf("%d\n",money);
 	}
 	return 0;
 } 
